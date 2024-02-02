a415725f4cef7c9c1e0e2dd828330ac6bc9e7828
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-02-02 10:26:35 +0100

    ADD- ventana principal.

--- app/src/main/java/com/example/examen_2/MainActivity.java
+++ app/src/main/java/com/example/examen_2/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.examen_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvConsulta;
+    static Integer cont = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvConsulta = findViewById(R.id.tvConsultas);
+    }
+
+    public void cargarConsulta(View view) {
+        Intent i = new Intent(this,SearchActivity.class);
+        startActivity(i);
+        tvConsulta.setText(cont+"");
+    }
+
+    public void cargarQuimico(View view) {
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void salir(View view) {
+        Toast.makeText(getApplicationContext(), "Adiós, vuelva pronto", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
